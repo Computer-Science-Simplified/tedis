ed3a19aba38005f9c78f474757bbf2c957dd3b27
martinjoo <martin@martinjoo.dev>
2024-08-16 02:40:04 +0200

    Refactor

--- server/cmd/migrate/main.go
+++ server/cmd/migrate/main.go
@@ -9,16 +9,18 @@ import (
 )
 
 func main() {
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 250000; i++ {
 		source := rand.NewSource(time.Now().UnixNano())
 
 		r := rand.New(source)
 
 		value := int64(r.Intn(1000000) + 1)
 
-		cmd, err := command.Create("bstadd", "a", []int64{value})
+		cmd, err := command.Create("BSTADD", "a", []int64{value})
 		if err != nil {
 			fmt.Println("could not create command")
+
+			continue
 		}
 
 		err = aol.Append(cmd)
