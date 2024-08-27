2d5ddbe492ee8fe7b872efeedc203ad31b8ed71c
martinjoo <martin@martinjoo.dev>
2024-08-27 17:57:37 +0200

    Fix variable name

--- server/internal/command/command.go
+++ server/internal/command/command.go
@@ -3,7 +3,7 @@ package command
 import "github.com/Computer-Science-Simplified/tedis/server/internal/types"
 
 type Command interface {
-	Execute(shouldReport bool) (string, error)
+	Execute(shouldFireEvent bool) (string, error)
 	GetParams() *types.CommandParams
 	String() string
 }
